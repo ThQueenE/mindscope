76652731b339314c2d97e9020bd4d804794c14f5
ThQueenE <lulia.luu.liii@gmail.com>
2025-10-18 19:53:47 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,37 +1,29 @@
-const CACHE_NAME = "mindscope-cache-v1";
+const CACHE_NAME = "mindscope-cache-v3";
 
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/assets/manifest-C0XrTpHF.json",
-  "/assets/icon-192.png",
-  "/assets/icon-512.png"
+  '/mindscope/',
+  '/mindscope/index.html',
+  '/mindscope/manifest.json',
+  '/mindscope/assets/icon-192.png',
+  '/mindscope/assets/icon-512.png',
+  '/mindscope/assets/icon-192-maskable.png',
+  '/mindscope/assets/icon-512-maskable.png'
 ];
 
 self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames
-          .filter(name => name !== CACHE_NAME)
-          .map(name => caches.delete(name))
-      );
-    })
+    caches.match(event.request).then(resp => resp || fetch(event.request))
   );
 });
